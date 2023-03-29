83bf30ef7db044f47fca18cef67ed1004e1c18bb
O. Eugenio <ome18@fsu.edu>
2023-03-29 02:18:33 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='PyDataOpsKit',
+    version='1.1.1',
+    packages=['PyDataOpsKit'],
+    url='',
+    license='',
+    author='Oli Eugenio',
+    author_email='',
+    description='',
+    install_requires=requirements,
+)
