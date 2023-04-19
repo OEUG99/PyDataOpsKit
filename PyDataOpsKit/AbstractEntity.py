d2ea2b917ea7c4f556cbae9a1627a1cf44121473
O. Eugenio <ome18@fsu.edu>
2023-04-19 01:34:43 -0400

    Added toDict and toJSON methods to the AbstractEntity class.

--- PyDataOpsKit/AbstractEntity.py
+++ PyDataOpsKit/AbstractEntity.py
@@ -1,7 +1,18 @@
+import json
 from abc import ABC, abstractmethod
 
 
 class AbstractEntity(ABC):
+    @abstractmethod
+    def toDict(self) -> dict:
+        """
+        Returns a dictionary representation of the object.
+        """
+        pass
+
+    def toJSON(self):
+        """Returns a JSON representation of the object"""
+        return json.dumps(self.toDict())
 
     def __repr__(self):
         return f'<{__class__.__name__}>'
